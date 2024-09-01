650e7fecd94f76026a9bd2f0012fc8c2bca8e01f
CapTaIN <maxmax.mishra@gmail.com>
2024-09-01 15:57:03 +0530

    New updates

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file have been successfully uploaded
+    console.log("uploaded on cloudinary", response.url);
+  } catch (error) {
+    fs.unlink(localFilePath); //remove the locally saved temp dile as upload been failed
+    return null;
+  }
+}
+export {uploadOnCloudinary}
